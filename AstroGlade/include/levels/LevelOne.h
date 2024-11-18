f792cabbf7ecb95fe1f0ababe96e10f9aba343c2
Void-11 <vivaanmishra777@gmail.com>
2024-11-18 23:44:38 +0530

    Game Level One Class

---
+++ AstroGlade/include/levels/LevelOne.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "framework/World.h"
+
+namespace ly
+{
+    class PlayerSpaceship;
+    class GameLevelOne : public World
+    {
+    public:
+        GameLevelOne(Application* owningApp);
+    private:
+        weak<PlayerSpaceship> testPlayerSpaceship;
+    };
+}
--- AstroGlade/src/gameFramework/GameApplication.cpp
+++ AstroGlade/src/gameFramework/GameApplication.cpp
@@ -2,9 +2,9 @@
 #include "framework/World.h"
 #include "framework/Actor.h"
 #include "framework/AssetManager.h"
-#include "player/PlayerSpaceship.h"
 #include "config.h"
 #include "enemy/Vanguard.h"
+#include "levels/LevelOne.h"
 
 ly::Application *GetApplication()
 {
@@ -19,25 +19,6 @@ namespace ly
         :Application{980,600,"Astro Glade", sf::Style::Titlebar | sf::Style::Close}
     {
         AssetManager::Get().SetAssetRootDirectory(GetResourceDir());
-        weak<World> newWorld = LoadWorld<World>();
-        testPlayerSpaceship = newWorld.lock()->SpawnActor<PlayerSpaceship>();
-        testPlayerSpaceship.lock()->SetActorLocation(sf::Vector2f(300.f,490.f));
-        testPlayerSpaceship.lock()->SetActorRotation(0.f);
-
-        weak<Vangaurd> testSpaceship = newWorld.lock()->SpawnActor<Vangaurd>();
-        testSpaceship.lock()->SetActorLocation(sf::Vector2f{100.f, 50.f});
-        counter = 0.f;
-    }
-
-    void GameApplication::Tick(float deltaTime)
-    {
-        counter += deltaTime;
-        // if(counter > 10.f)
-        // {
-        //     if(!testPlayerSpaceship.expired())
-        //     {
-        //         testPlayerSpaceship.lock()->Destroy();
-        //     }
-        // }
+        weak<GameLevelOne> newWorld = LoadWorld<GameLevelOne>();
     }
 }
