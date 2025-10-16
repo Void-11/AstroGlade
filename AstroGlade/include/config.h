6464f39835ea434545502d4041fa07a7d3482e0f
Void-11 <vivaanmishra777@gmail.com>
2025-10-16 22:10:01 +0530

    Build Fixes

--- AstroGlade/include/config.h
+++ AstroGlade/include/config.h
@@ -6,6 +6,6 @@ std::string GetResourceDir()
 #ifdef NDEBUG //release build
 	return "assets/";
 #else
-	return "F:/C++/C++ Projects/LightYears-master/AstroGlade/assets/";
+	return "F:/C++/C++ Projects/AstroGlade/AstroGlade/assets/";
 #endif
 }
